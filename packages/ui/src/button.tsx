ac6e36073d231f2be0baf72fbc2bd8c7e6be0365
George Bockari <bitwhys@gmail.com>
2023-11-24 21:26:41 -0500

    refactor(button): refactor Button component to use forwardRef

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,14 +1,17 @@
+import React, { forwardRef } from "react";
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function Button({ children, ...other }: ButtonProps): JSX.Element {
-  return (
-    <button type="button" {...other}>
-      {children}
-    </button>
-  );
-}
-
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  function Button({ children, ...other }: ButtonProps, ref): JSX.Element {
+    return (
+      <button type="button" {...other} ref={ref}>
+        {children}
+      </button>
+    );
+  },
+);
 Button.displayName = "Button";
